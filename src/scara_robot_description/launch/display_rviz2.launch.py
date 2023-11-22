51f2d4cb5b14d29559584427c0a2dbcfcd679c16
NotCuteButNew <bwd2386884119@gmail.com>
2023-11-22 11:06:48 +0800

    ✨ feat: launch启动rviz并代入scara

---
+++ src/scara_robot_description/launch/display_rviz2.launch.py
@@ -0,0 +1,42 @@
+import os
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    package_name = 'scara_robot_description'
+    urdf_name = "scara_robot.urdf"
+    scara_rviz_config_name='scara_rviz.rviz'
+    
+    ld = LaunchDescription()
+    pkg_share = FindPackageShare(package=package_name).find(package_name) 
+    urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
+    scara_rviz_config_path=os.path.join(pkg_share, f'rviz/{scara_rviz_config_name}')
+    
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        arguments=[urdf_model_path]
+        )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        arguments=[urdf_model_path]
+        )
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d',scara_rviz_config_path],
+        output='screen'
+        )
+
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(joint_state_publisher_node)
+    ld.add_action(rviz2_node)
+
+    return ld
--- src/scara_robot_description/setup.py
+++ src/scara_robot_description/setup.py
@@ -1,15 +1,20 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'scara_robot_description'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/**')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/**'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
