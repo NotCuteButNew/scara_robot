7292b0712e2314145c669c4733952290449a7624
NotCuteButNew <bwd2386884119@gmail.com>
2023-11-22 21:28:38 +0800

    ✨ feat: 新增scara启动gazebo的launch

---
+++ src/scara_robot_description/launch/gazebo.launch.py
@@ -0,0 +1,44 @@
+import os
+import xacro
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    robot_name_in_model = 'scara_robot'
+    package_name = 'scara_robot_description'
+    xacro_name = "gazebo.xacro"
+
+    ld = LaunchDescription()
+    pkg_share = FindPackageShare(package=package_name).find(package_name) 
+    urdf_model_path = os.path.join(pkg_share, f'urdf/{xacro_name}')
+    doc=xacro.process_file(urdf_model_path)
+    robot_desc=doc.toprettyxml(indent=' ')
+    params = {'robot_description': robot_desc}
+    
+    start_gazebo_cmd =  ExecuteProcess(
+        cmd=['gazebo', '--verbose','-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+        output='screen')
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[params]
+        )
+    
+    # Launch the robot
+    spawn_entity_cmd = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=['-entity', robot_name_in_model, '-z','1','-topic', '/robot_description'], 
+        output='screen',
+        parameters=[params]
+        )
+
+    ld.add_action(start_gazebo_cmd)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(spawn_entity_cmd)
+
+    return ld
