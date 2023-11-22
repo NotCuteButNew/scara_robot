9400f7def18d98a867cbc8ae184f1282bfb59bc0
NotCuteButNew <bwd2386884119@gmail.com>
2023-11-22 16:08:21 +0800

    🦄 refactor: 采用xacro

--- src/scara_robot_description/launch/display_rviz2.launch.py
+++ src/scara_robot_description/launch/display_rviz2.launch.py
@@ -1,4 +1,5 @@
 import os
+import xacro
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
@@ -6,25 +7,28 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     package_name = 'scara_robot_description'
-    urdf_name = "scara_robot.urdf"
+    xacro_name = "scara.xacro"
     scara_rviz_config_name='scara_rviz.rviz'
     
     ld = LaunchDescription()
     pkg_share = FindPackageShare(package=package_name).find(package_name) 
-    urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
+    urdf_model_path = os.path.join(pkg_share, f'urdf/{xacro_name}')
+    doc=xacro.process_file(urdf_model_path)
+    robot_desc=doc.toprettyxml(indent=' ')
+    params = {'robot_description': robot_desc}
     scara_rviz_config_path=os.path.join(pkg_share, f'rviz/{scara_rviz_config_name}')
     
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        arguments=[urdf_model_path]
+        parameters=[params]
         )
 
     joint_state_publisher_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
-        arguments=[urdf_model_path]
+        parameters=[params]
         )
 
     rviz2_node = Node(
